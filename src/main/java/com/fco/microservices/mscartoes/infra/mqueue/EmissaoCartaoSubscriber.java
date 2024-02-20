49151f647bc2be3b7954956c8881513d25d5bf24
Francisco Junior <jrpires.git@gmail.com>
2024-02-20 18:10:16 -0400

    add log em emissao de cartao

--- src/main/java/com/fco/microservices/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ src/main/java/com/fco/microservices/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -7,12 +7,14 @@ import com.fco.microservices.mscartoes.domain.Cartao;
 import com.fco.microservices.mscartoes.domain.ClienteCartao;
 import com.fco.microservices.mscartoes.infra.repository.CartaoRepository;
 import com.fco.microservices.mscartoes.infra.repository.ClienteCartaoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     @Autowired
@@ -37,7 +39,7 @@ public class EmissaoCartaoSubscriber {
             clienteCartaoRepository.save(clienteCartao);
 
         }catch (Exception e) {
-            e.printStackTrace();
+            log.error("Erro ao processar emissao de cartao: {} ", e.getMessage());
         }
     }
 }
